c79239540383335542935b3778b99ac821206249
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-07-21 08:31:52 -0600

    Added new e2e test

--- cypress/integration/index.e2e.test.ts
+++ cypress/integration/index.e2e.test.ts
@@ -3,8 +3,15 @@ describe("index page", () => {
     cy.visit("/")
   })
 
-  it("Goes to the Create Game Page", () => {
+  it.skip("Enters in a Username and Clicks Create Private Game", () => {
+    cy.wait(2000)
+    cy.location("pathname").should("eq", "/")
     cy.contains("h2", "Create Game")
+    const usernameInput = cy.get("form")
+    usernameInput.type("Mellow")
+    usernameInput.should("have.value", "Mellow")
+    cy.get("#create-private-game-btn").click()
+    cy.location("pathname").should("contain", "/games/")
   })
 })
 
