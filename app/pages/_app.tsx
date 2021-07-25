cebae63736c234cecad3c52559e73871f2f583e3
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-07-24 19:46:24 -0600

    Added Head

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -6,6 +6,7 @@ import {
   ErrorComponent,
   ErrorFallbackProps,
   getAntiCSRFToken,
+  Head,
   useQueryErrorResetBoundary,
 } from "blitz"
 import LoginForm from "app/auth/components/LoginForm"
@@ -38,6 +39,16 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <AnimateSharedLayout>
+      <Head>
+        <meta property="og:title" content="Shiritori" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://codertheory.dev" />
+        <meta
+          property="og:image"
+          content="https://image.spreadshirtmedia.com/image-server/v1/compositions/T813A2PA3132PT17X34Y52D1000021407FS6656/views/1,width=650,height=650,appearanceId=2,backgroundColor=ffffff.jpg"
+        />
+        <title>Shiritori</title>
+      </Head>
       <PusherProvider {...config}>
         <ChakraProvider theme={theme}>
           <ErrorBoundary
