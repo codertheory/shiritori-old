998a9af66cf2ad697ccde05d60dd35429d2ce87d
Lucas <13612932+LucasCoderT@users.noreply.github.com>
2021-08-04 08:37:19 -0600

    Updated index.e2e.test.ts

--- cypress/integration/index.e2e.test.ts
+++ cypress/integration/index.e2e.test.ts
@@ -3,11 +3,11 @@ describe("index page", () => {
     cy.visit("/")
   })
 
-  it.skip("Enters in a Username and Clicks Create Private Game", () => {
+  it("Enters in a Username and Clicks Create Private Game", () => {
     cy.wait(2000)
     cy.location("pathname").should("eq", "/")
-    cy.contains("h2", "Create Game")
-    const usernameInput = cy.get("form")
+    cy.contains("h2", "Test Your").should("exist")
+    const usernameInput = cy.get("input")
     usernameInput.type("Mellow")
     usernameInput.should("have.value", "Mellow")
     cy.get("#create-private-game-btn").click()
